7484208e720517e1c4a6e03a694a685e05e723a3
mmieses01 <manuel.mieses48@gmail.com>
2022-11-21 11:07:30 -0800

    intro

---
+++ index.js
@@ -0,0 +1,8 @@
+var superheroes = require("superheroes");
+var mysuperheroename = superheroes.random();
+
+const supervillains = require('supervillains');
+var myvilliam = supervillains.random();
+
+console.log("super heroe: "+mysuperheroename);
+console.log("Villano: "+myvilliam);
